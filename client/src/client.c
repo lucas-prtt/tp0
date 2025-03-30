84eb8b857b77c90a49726b7feea8e0e9ba3efaf6
lucas-prtt <lpresotto@frba-utn-edu.ar>
2025-03-30 14:16:29 +0000

    Log and config

--- client/src/client.c
+++ client/src/client.c
@@ -1,5 +1,6 @@
 #include "client.h"
-
+#include <commons/log.h>
+#include <readline/readline.h>
 int main(void)
 {
 	/*---------------------------------------------------PARTE 2-------------------------------------------------------------*/
@@ -7,7 +8,7 @@ int main(void)
 	int conexion;
 	char* ip;
 	char* puerto;
-	char* valor;
+	char* clave;
 
 	t_log* logger;
 	t_config* config;
@@ -15,20 +16,31 @@ int main(void)
 	/* ---------------- LOGGING ---------------- */
 
 	logger = iniciar_logger();
-
+	if(logger == NULL){
+		abort();
+	}
 	// Usando el logger creado previamente
 	// Escribi: "Hola! Soy un log"
-
+	log_info(logger, "Soy un log!");
 
 	/* ---------------- ARCHIVOS DE CONFIGURACION ---------------- */
 
 	config = iniciar_config();
-
+	if(config == NULL){
+		abort();
+	}
+	clave = config_get_string_value(config, "CLAVE");
+	ip = config_get_string_value(config, "IP");
+	puerto = config_get_string_value(config, "PUERTO");
 	// Usando el config creado previamente, leemos los valores del config y los 
 	// dejamos en las variables 'ip', 'puerto' y 'valor'
 
 	// Loggeamos el valor de config
-
+	log_info(logger, "CLAVE:");
+	log_info(logger, clave);
+	log_info(logger, "IP y puerto");
+	log_info(logger, ip);
+	log_info(logger, puerto);
 
 	/* ---------------- LEER DE CONSOLA ---------------- */
 
@@ -55,14 +67,14 @@ int main(void)
 t_log* iniciar_logger(void)
 {
 	t_log* nuevo_logger;
-
+	nuevo_logger = log_create("tp0.log", "tp0", true, LOG_LEVEL_INFO);
 	return nuevo_logger;
 }
 
 t_config* iniciar_config(void)
 {
 	t_config* nuevo_config;
-
+	nuevo_config = config_create("cliente.config");
 	return nuevo_config;
 }
 
@@ -95,6 +107,7 @@ void paquete(int conexion)
 
 void terminar_programa(int conexion, t_log* logger, t_config* config)
 {
+	log_destroy(logger);
 	/* Y por ultimo, hay que liberar lo que utilizamos (conexion, log y config) 
 	  con las funciones de las commons y del TP mencionadas en el enunciado */
 }
